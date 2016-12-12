6514ef6cc77d49be22b683e423b0cd4d8b90c5d8
guillaume.garcia@telecomnancy.net <guillaume.garcia@telecomnancy.net>
2016-12-12 13:17:07 +0100

    Ajout bouton search et premier label

--- src/main/java/codingweek2016/App.java
+++ src/main/java/codingweek2016/App.java
@@ -2,12 +2,21 @@ package codingweek2016;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class App extends JFrame implements ActionListener {
+	
+    private JLabel text = new JLabel("Vidéo à chercher :");
+    private JButton search = new JButton("Search");
 	
 	public App() {
 		super("Youtube Copycat");
@@ -15,9 +24,21 @@ public class App extends JFrame implements ActionListener {
 				
 		setLayout(new BorderLayout());
 		
+        text.setHorizontalAlignment(SwingConstants.CENTER);
+        Font sensorValueFont = new Font("Sans Serif", Font.BOLD, 18);
+        text.setFont(sensorValueFont);
+
+        this.add(text, BorderLayout.CENTER);
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		pack();
 		setVisible(true);
+		
+        JPanel buttonsPanel = new JPanel();
+        buttonsPanel.setLayout(new GridLayout(1, 1));
+        buttonsPanel.add(search);        
+        this.add(buttonsPanel, BorderLayout.SOUTH);
+
 	}
 
 	public static void main(String[] args) {
@@ -27,7 +48,6 @@ public class App extends JFrame implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
-		//dfgdhdgh
 		
 	}
 

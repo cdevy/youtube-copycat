29a6b2758182d3fa3fc81a1ed3532bf6342f9106
cdevy <c.devy95@gmail.com>
2016-12-13 14:19:07 +0100

    Ajout classe Video

--- src/main/java/codingweek2016/features/Model.java
+++ src/main/java/codingweek2016/features/Model.java
@@ -13,8 +13,10 @@ import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.services.samples.youtube.cmdline.Auth;
 import com.google.api.services.samples.youtube.cmdline.data.Search;
 import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.ResourceId;
 import com.google.api.services.youtube.model.SearchListResponse;
 import com.google.api.services.youtube.model.SearchResult;
+import com.google.api.services.youtube.model.Thumbnail;
 
 public class Model extends Observable {
 	
@@ -28,6 +30,15 @@ public class Model extends Observable {
 		super();
 	}
 	
+	public List<Video> loadVideos(List<SearchResult> results) {
+		List<Video> videos2 = new ArrayList<Video>();
+		for (int i=0;i<results.size();i++) {
+			SearchResult singleVideo = results.get(i);
+			videos2.add(new Video(singleVideo));    
+        }
+		return videos2;
+	}
+	/*
 	public void searchKeyWord(String userInput) {
         Properties properties = new Properties();
         try {
@@ -64,7 +75,7 @@ public class Model extends Observable {
             SearchListResponse searchResponse = search.execute();
             videos = searchResponse.getItems();
             System.out.println("");
-            /* Notify observer of changes */
+            /* Notify observer of changes 
             setChanged();
             notifyObservers(videos);
             
@@ -76,6 +87,53 @@ public class Model extends Observable {
         } catch (Throwable t) {
             t.printStackTrace();
         }
+	}*/
+	
+	public List<SearchResult> searchKeyWord(String userInput) {
+        Properties properties = new Properties();
+        try {
+            InputStream in = Search.class.getResourceAsStream("/" + PROPERTIES_FILENAME);
+            properties.load(in);
+
+        } catch (IOException e) {
+            System.err.println("There was an error reading " + PROPERTIES_FILENAME + ": " + e.getCause()
+                    + " : " + e.getMessage());
+            System.exit(1);
+        }
+        
+        try {
+
+            youtube = new YouTube.Builder(Auth.HTTP_TRANSPORT, Auth.JSON_FACTORY, new HttpRequestInitializer() {
+            	
+                public void initialize(HttpRequest request) throws IOException {
+                	
+                }
+            }).setApplicationName("youtube-cmdline-search-sample").build();
+
+            String queryTerm = userInput;
+            
+            YouTube.Search.List search = youtube.search().list("id,snippet");
+
+            String apiKey = properties.getProperty("youtube.apikey");
+            search.setKey(apiKey);
+            search.setQ(queryTerm);
+            System.out.println("");
+            search.setType("video");
+
+            search.setFields("items(id/kind,id/videoId,snippet/title,snippet/thumbnails/default/url)");
+            search.setMaxResults(NUMBER_OF_VIDEOS_RETURNED);
+            SearchListResponse searchResponse = search.execute();
+            return(searchResponse.getItems());
+            
+        } catch (GoogleJsonResponseException e) {
+            System.err.println("There was a service error: " + e.getDetails().getCode() + " : "
+                    + e.getDetails().getMessage());
+        } catch (IOException e) {
+            System.err.println("There was an IO error: " + e.getCause() + " : " + e.getMessage());
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+		return null;
 	}
 	
 	public List<SearchResult> getVideos() {
---
+++ src/main/java/codingweek2016/features/Video.java
@@ -0,0 +1,112 @@
+package codingweek2016.features;
+
+import java.awt.BorderLayout;
+import java.awt.ComponentOrientation;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import com.google.api.services.youtube.model.SearchResult;
+
+public class Video extends JPanel {
+	
+	private JButton thumbnail = new JButton();
+	private JButton title = new JButton();
+	private String url = "https://www.youtube.com/watch?v=";
+	
+	public Video(SearchResult result) {
+		super();
+		this.setLayout(new BorderLayout());
+		
+		url += result.getId().getVideoId();
+		
+		try {
+			thumbnail.setIcon(new ImageIcon(new URL(result.getSnippet().getThumbnails().getDefault().getUrl())));
+		} catch (Exception e) {
+		    System.out.println(e);
+		}
+		thumbnail.addActionListener(new ActionListener() {
+			  
+            public void actionPerformed(ActionEvent e) {
+            	System.out.println("Read video");
+            }
+        });
+		thumbnail.setOpaque(false);
+		thumbnail.setContentAreaFilled(false);
+		thumbnail.setBorderPainted(false);
+		thumbnail.setPreferredSize(new Dimension(200,0));
+		this.add(thumbnail, BorderLayout.WEST);
+		
+		title.setText(result.getSnippet().getTitle());
+		title.addActionListener(new ActionListener() {
+			  
+            public void actionPerformed(ActionEvent e) {
+            	System.out.println("Read video");
+            }
+        });
+		title.setOpaque(false);
+		title.setContentAreaFilled(false);
+		title.setBorderPainted(false);
+		this.add(title);
+	}
+	
+	public void display() {
+		
+	}
+	
+	public static void main(String args[]) {
+		JFrame frame = new JFrame("");
+		frame.setLayout(new BorderLayout());
+		frame.setPreferredSize(new Dimension(900,700));
+		
+		JPanel panel = new JPanel();
+		GridLayout grid = new GridLayout(10,1);
+		grid.setVgap(10);
+		panel.setLayout(grid);
+		List<SearchResult> results = new ArrayList<SearchResult>();
+		JTextArea resultPane = new JTextArea();
+		
+		Model m = new Model();
+		
+    	results = m.searchKeyWord("lol");
+    	System.out.println(results);
+		    	
+    	List<Video> videos = new ArrayList<Video>(); 
+    	videos = m.loadVideos(results);
+    	System.out.println(videos);
+    	
+    	resultPane.setOpaque(false);
+	    
+		if (videos != null) {
+			if (!videos.iterator().hasNext()) {
+				resultPane.append("There aren't any results for your query.\n");
+			} else {
+		    	resultPane.append("=============================================================\n");
+		    	resultPane.append("First " + 3 + " videos for search on \"" + "lol" + "\".\n");
+		    	resultPane.append("=============================================================\n");
+		   	}
+		}
+		
+		for (int i=0; i<videos.size(); i++) {
+    		panel.add(videos.get(i));
+    	}
+		
+		frame.add(resultPane, BorderLayout.NORTH);
+		frame.add(panel, BorderLayout.CENTER);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+	}
+}

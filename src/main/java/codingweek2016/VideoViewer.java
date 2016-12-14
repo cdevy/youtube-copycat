b6f3aad40b0f19692c397599d268905d0ea1f2a4
Patmosis <gauthier.zambaux@outlook.com>
2016-12-14 10:15:27 +0100

    Modif de VideoViewer

--- src/main/java/codingweek2016/VideoViewer.java
+++ src/main/java/codingweek2016/VideoViewer.java
@@ -7,7 +7,12 @@ import javax.swing.JFrame;
 
 import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
@@ -35,9 +40,17 @@ public class VideoViewer extends JFrame {
 	}
 	
 	private static void initFX(JFXPanel jfxPanel, String id) {
+		/*StringBuilder sb = new StringBuilder();
+	    sb.append("<embed width=\"420\" height=\"315\"src=\"http://www.youtube.com/embed/"+id+"?autoplay=1\">");//("<video width=\"320\" height=\"240\" controls><source src=\"https://www.youtube.com/embed/"+id+"?autoplay=1\" type=\"video/mp4\"></video>");
+	    String fontText = sb.toString();
+	    WebView fontWebView = new WebView();;
+		fontWebView.getEngine().loadContent(fontText);
+		jfxPanel.setScene(new Scene(fontWebView));*/
+		
+		
 		WebView view = new WebView();
 		WebEngine webEngine = view.getEngine();
-		webEngine.load("http://www.youtube.com/embed/"+id+"?autoplay=0");
+		webEngine.load("http://www.youtube.com/embed/"+id+"?autoplay=1");
 		view.setPrefSize(200, 200);
         jfxPanel.setScene(new Scene(view));
  	}

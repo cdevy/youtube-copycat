aed8e3cc8049b81e2b92456117b1357c86787332
cdevy <c.devy95@gmail.com>
2016-12-14 21:23:03 +0100

    Mise à jour VideoViewer

--- src/main/java/codingweek2016/VideoViewer.java
+++ src/main/java/codingweek2016/VideoViewer.java
@@ -1,22 +1,9 @@
 package codingweek2016;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.awt.SWT_AWT;
 import org.eclipse.swt.browser.Browser;
-import org.eclipse.swt.layout.RowLayout;
-import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
-
-import java.awt.FlowLayout;
-import java.io.IOException;
-import java.util.List;
-
-import javax.swing.JButton;
-import javax.swing.JPanel;
-import javax.swing.JTextArea;
-
-import com.google.api.services.youtube.model.CommentThread;
 
 public class VideoViewer {
 	
@@ -26,46 +13,12 @@ public class VideoViewer {
 
         Display display = new Display();
         Shell shell = new Shell(display);
-        shell.setBounds(20,20,700,600);
+        shell.setBounds(20,20,700,628);
         shell.setText(title);
 
         Browser browser = new Browser(shell, SWT.NONE);
-        browser.setBounds(100,30,500,300);
+        browser.setBounds(0,0,700,600);
         browser.setUrl(url);
-        
-        // ESSAI POUR INTEGRER UN JPANEL
-        Composite cmp = new Composite(shell, SWT.EMBEDDED);
-        cmp.setLayout(new RowLayout());
-        
-        java.awt.Frame frame = SWT_AWT.new_Frame(cmp);
-        java.awt.Panel panel = new java.awt.Panel(new java.awt.BorderLayout());
-        
-        final javax.swing.JPanel commentsArea = new javax.swing.JPanel();
-        
-        JPanel createComment = new JPanel();
-        createComment.setLayout(new FlowLayout());
-        JTextArea commentEntry = new JTextArea();
-        createComment.add(commentEntry);
-        JButton publish = new JButton("publish");
-        createComment.add(publish);
-        commentsArea.add(createComment);
-        
-        
-        /*
-        Comment com = new Comment(id);
-        try {
-			List<CommentThread> comments = com.getComments();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
-        
-        
-        panel.add(commentsArea);
-        frame.add(panel);
-        
-        // FIN ESSAI POUR INTEGRER UN JPANEL
-        
         
         shell.open();
 

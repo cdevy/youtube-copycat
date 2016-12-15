b892d3e8d26da252d66bf8a4a60f61784b68f790
cdevy <c.devy95@gmail.com>
2016-12-15 17:07:13 +0100

    Lien commentaires et profiles

--- src/main/java/codingweek2016/CommentsWindow.java
+++ src/main/java/codingweek2016/CommentsWindow.java
@@ -2,6 +2,10 @@ package codingweek2016;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,22 +21,38 @@ import com.google.api.services.youtube.model.CommentThread;
 
 import codingweek2016.model.Comment;
 
-
-
 @SuppressWarnings("serial")
 public class CommentsWindow extends JFrame {
 	
+	private JTextArea commentEntry;
 
 	public CommentsWindow(String title, final String id){
 		super("Comments on " + title);
-		this.setPreferredSize(new Dimension(700,1000));
+		this.setPreferredSize(new Dimension(1200,1000));
 		
 		this.setLayout(new BorderLayout(30, 30));
 		
 		JPanel createComment = new JPanel();
         createComment.setLayout(new BoxLayout(createComment, BoxLayout.X_AXIS));
         
-        JTextArea commentEntry = new JTextArea("Enter here your comment"); 
+        commentEntry = new JTextArea("Write your comment here"); 
+        commentEntry.setEnabled(false);
+        commentEntry.addMouseListener(new MouseAdapter() {
+        	public void mouseClicked(MouseEvent event) {
+        		commentEntry.setEnabled(true);
+        		commentEntry.requestFocus();
+            }
+        });
+        commentEntry.addFocusListener(new FocusListener() {
+			public void focusGained(FocusEvent e) {
+				commentEntry.setText("");
+			}
+
+			public void focusLost(FocusEvent e) {
+				commentEntry.setText("Write your comment here");
+			}
+        	
+        });
         createComment.add(commentEntry);
         
         JButton publish = new JButton("publish");

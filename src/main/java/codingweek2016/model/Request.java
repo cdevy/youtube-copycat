1d4cc9c5dc0a9ab7ca064de76d762bee53bd7ff5
maud-farizon <maud.farizon@telecomnancy.net>
2016-12-15 21:35:24 +0100

    changement du nombre de vidéo affcihées

--- src/main/java/codingweek2016/model/Request.java
+++ src/main/java/codingweek2016/model/Request.java
@@ -11,7 +11,6 @@ import com.google.api.services.youtube.YouTube;
 public abstract class Request extends Observable {
 	
 	protected static final String PROPERTIES_FILENAME = "youtube.properties";
-	protected static final long NUMBER_OF_VIDEOS_RETURNED = 25;
 	protected static YouTube youtube;
 	protected String apiKey;
 	

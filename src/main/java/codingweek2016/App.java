1b17001962d1dd370304a9e24fc2c489f8a353c7
guillaume.garcia@telecomnancy.net <guillaume.garcia@telecomnancy.net>
2016-12-16 11:35:03 +0100

    Verification nom d'executable avant de lancer l'app (NE PAS RENOMMER LE JAR POUR EXECUTER

--- src/main/java/codingweek2016/App.java
+++ src/main/java/codingweek2016/App.java
@@ -1,5 +1,7 @@
 package codingweek2016;
 
+import java.io.File;
+
 import codingweek2016.MainWindow;
 
 public class App {
@@ -7,6 +9,11 @@ public class App {
 	public static long NUMBER_OF_VIDEOS_RETURNED = 25;
 	
 	public static void main(String[] args) {
+		//Vérifie le nom/existence du jar !
+		if (!new File("youtubeCopycat.jar").exists()) {
+		    throw new RuntimeException("L'exécutable doit se nommer \"youtubeCopycat.jar\" !");
+		}
+		//Lance l'appli à proprement parler
 		new MainWindow();
 	}
 }
